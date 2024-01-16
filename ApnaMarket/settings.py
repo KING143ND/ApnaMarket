6571356b5635e348b140ca03351949f933883b1f
KING143ND <nvnaveenchaudhary@gmail.com>
2024-01-16 13:55:01 +0530

    vercel deployment

--- ApnaMarket/settings.py
+++ ApnaMarket/settings.py
@@ -90,19 +90,19 @@ WSGI_APPLICATION = 'ApnaMarket.wsgi.application'
 
 DATABASES = {
     #========================== Sqlite3 database ==========================
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
+    #  'default': {
+    #      'ENGINE': 'django.db.backends.sqlite3',
+    #      'NAME': BASE_DIR / 'db.sqlite3',
+    #  }
     #========================== Postgresql databse ==========================
-    # 'default': {
-    #    'ENGINE': os.getenv('DB_ENGINE'),
-    #    'NAME': os.getenv('DB_NAME'),
-    #    'USER': os.getenv('DB_USER'),
-    #    'PASSWORD': os.getenv('DB_PASSWORD'),
-    #    'HOST': os.getenv('DB_HOST'),
-    #    'PORT': os.getenv('DB_PORT'),
-    # }
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'ApnaMarketDB',
+       'USER': 'naveen',
+       'PASSWORD': 'king143nd',
+       'HOST': 'localhost',
+       'PORT': '5432',
+    }
 }
 
 
