23912cd1bd72bedb33815918ed1b07069d9bdcf7
KING143ND <nvnaveenchaudhary@gmail.com>
2024-01-16 14:16:49 +0530

    vercel deployment

--- ApnaMarket/settings.py
+++ ApnaMarket/settings.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
